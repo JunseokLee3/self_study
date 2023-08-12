fab83795e1f8bc964ec9195c96303146da64e11e
Junseok <leeis327@naver.com>
2023-08-12 12:21:24 +0900

    push

--- coding test/연습문제/5-9.py
+++ coding test/연습문제/5-9.py
@@ -4,4 +4,19 @@
  
  # BFS 메서드 정의
  def bfs(graph, start, visited):
-     
+     # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+     queue = deque([start])
+     # 현재 노드를 방문 처리
+     visited([start]) = True
+     # 큐가 빌 떄까지 반복
+     while queue:
+         # 큐에서 하나의 원소를 뽑아 출력
+         v = queue.popleft()
+         print(v, end = ' ')
+         # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+         for i in graph(v):
+             if not visited[i]:
+                 queue.append(i)
+                 visited[i] = True
+                 
+# 각 노드가 ㅇ
