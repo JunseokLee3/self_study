eaaebc1197a0913af0ec904a29d999f9046b328f
JunseokLEE <leeis327@naver.com>
2023-08-17 17:50:42 +0900

    2023-08-17

--- coding test/연습문제 1회독/7-4.py
+++ coding test/연습문제 1회독/7-4.py
@@ -1,8 +1,20 @@
-## 한 줄 입력받아 출력하는 소스코드
 
-import sys
-# 하나의 문자열 데이터 입력받기
-input_data = sys.stdin.readline().rstrip()
 
-# 입력받은 문자열 그대로 출력
-print(input_data)
+
+
+
+
+
+
+
+
+
+
+
+
+# N(가게의 부품 개수) 입력
+n = int(input())
+# 가게에 있는 전체 부품 번호를 공백으로 구분하여 입력
+array = list(map(int, input().split()))
+array.sort() # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# M(손님이 확인 요청한 부품 개수) 입력
