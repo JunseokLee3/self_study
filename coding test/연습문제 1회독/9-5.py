ddb2f2c4f0fef0ac409b5598da57c1451281c8b0
JunseokLEE <leeis327@naver.com>
2023-08-24 16:10:23 +0900

    2023-08-24

---
+++ coding test/연습문제 1회독/9-5.py
@@ -0,0 +1,9 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
+
+# 노드의 개수, 간선의 개수, 시작 노드를 입력받기
+n, m, start = map(int, input().split())
+# 각 노드에 연결되어 잇는 노드에 대한 정보를 담는 리스트를 만들기
