ea3a3007b8de7f7dbb356ba9a0dd323eac3da167
JunseokLEE <leeis327@naver.com>
2023-09-27 18:56:33 +0900

    u

---
+++ coding test/2회독/연습문제/3-1.py
@@ -0,0 +1,13 @@
+n = 1260
+count = 0
+
+# 큰 단위의 화폐부터 차례대로 확인
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+    count += n // coin # 해당 화폐로 거슬로 줄 수 있는 동전의 개수 세기
+    
+    n %= coin
+    print('count :', count, n)
+
+print(count)
