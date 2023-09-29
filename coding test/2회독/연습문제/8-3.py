43fe8ba77d92062e4c18f369c49fe121fabec732
JunseokLEE <leeis327@naver.com>
2023-09-29 11:44:28 +0900

    u

---
+++ coding test/2회독/연습문제/8-3.py
@@ -0,0 +1,28 @@
+d = [0] * 100
+
+def pibo(x):
+    print('f(' + str(x) + ')', end = ' ')
+    if x == 1 or x ==2 :
+        return 1
+    
+    if d[x] != 0:
+        return d[x]
+    d[x] = pibo(x-1) + pibo(x-2)
+    return d[x]
+
+pibo(6)
+
+
+# 앞서 계산된 결과를 저장힉 위한 DP 테이블 초기화
+d = [0] * 100
+
+# 첫 번째 피보나치 수와 두 번째 피보나치 수느 1
+d[1] = 1
+d[2] = 1
+n = 99
+
+# 피보나치 함수(Fibonacci Function) 반복문으로 구현(보텀업 다이나믹 프로그래밍)
+for i in range(3, n + 1):
+    d[i] =  d[i -1] + d[i -2]
+
+print(d[n])
