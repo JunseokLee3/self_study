5ceed8eb087b6e2ab3506252df0e257213a76016
Junseok LEE <leeis327@naver.com>
2023-10-01 00:32:27 +0900

    u

--- coding test/2회독/실전문제 2회/13/2.py
+++ coding test/2회독/실전문제 2회/13/2.py
@@ -16,4 +16,38 @@ def virus(x, y):
         ny = y + dy[i]
         if nx >= 0 and nx < n and ny >= 0 and ny < m:
             if temp[nx][ny] ==0:
-                temp[nx][ny]
+                temp[nx][ny] = 2
+                virus(nx, ny)
+
+def get_score():
+    score = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] == 0:
+                score += 1
+    return score
+
+def dfs(count):
+    global result
+    if count == 3:
+        for i in range(n):
+            for j in range(m):
+                temp[i][j] = data[i][j]
+        for i in range(n):
+            for j in range(m):
+                if temp[i][j] == 2:
+                    virus(i,j)
+        result = max(result, get_score())
+        return
+    
+    for i in range(n):
+        for j in range(m):
+            if data[i][j] == 0:
+                data[i][j] =1
+                count += 1
+                dfs(count)
+                data[i][j]=0
+                count -= 1
+
+dfs(0)
+print(result)
