c506a785a705755b434b31b932f1abe910c359d0
JunseokLEE <leeis327@naver.com>
2023-10-19 15:58:21 +0900

    u

--- coding test/기출/2023/하반기/오후/1.py
+++ coding test/기출/2023/하반기/오후/1.py
@@ -1,19 +1,19 @@
 def is_inrange(x, y):
-    return 1 <= x and x <= n and 1 <= y and y <= n
+    return 1 <= x and x <= n and 1 <= y and y <= n 
 
 n, m, p, c, d = map(int, input().split())
 rudolf = tuple(map(int, input().split()))
 
-points = [0 for _ in range(p+1)]
-pos = [(0,0) for _ in range(p+1)]
-board = [[0 for _ in range(n+1)] for _ in range(n +1)]
+points = [0 for _ in range(p + 1)]
+pos = [(0 ,0) for _ in range(p + 1)]
+board = [[0 for _ in range(n+1)] for _ in range(n+1)]
 is_live = [False for _ in range(p +1)]
 stun = [0 for _ in range(p+1)]
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
-board[rudolf[0]][rudolf[1]] = -1
+board[rudolf[0]][rudolf[1]] = - 1
 
 for _ in range(p):
     id, x, y = tuple(map(int, input().split()))
@@ -23,17 +23,16 @@ for _ in range(p):
 
 for t in range(1, m + 1):
     closestX, closestY, closestIdx = 10000, 10000, 0
-
     for i in range(1, p + 1):
         if not is_live[i]:
             continue
 
-        currentBest = ((closestX, rudolf[0]) ** 2 + (closestY - rudolf[1]) ** 2, (-closestX, -closestY))
-        currentValue = ((pos[i][0] - rudolf[0]) ** 2 + (pos[i][1] - rudolf[1]) ** 2, (-pos[i][0], -pos[i][1]))
+        currentBest = ((closestX - rudolf[0]) ** 2 + (closestY - rudolf[1]) **2, (-closestX, -closestY))
+        currentValue = ((pos[i][0] - rudolf[0]) **2 + (pos[i][1] - rudolf[1]) ** 2, (-pos[i][0], -pos[i][1]))
 
         if currentValue < currentBest:
             closestX, closestY = pos[i]
-            colsestIdx = i
+            closestIdx = i
 
     if closestIdx:
         prevRudolf = rudolf
@@ -53,20 +52,20 @@ for t in range(1, m + 1):
         board[prevRudolf[0]][prevRudolf[1]] = 0
 
     if rudolf[0] == closestX and rudolf[1] == closestY:
-        firstX = closestX + moveX * c
+        firstX = closestX + moveY * c
         firstY = closestY + moveY * c
         lastX, lastY = firstX, firstY
 
         stun[closestIdx] = t + 1
 
-        while is_inrange(lastX, lastY) and board[lastX][lastY] > 0:
+        while is_inrange(lastX, lastY) and  board[lastX][lastY] > 0:
             lastX += moveX
             lastY += moveY
-        
+
         while not (lastX == firstX and lastY == firstY):
             beforeX = lastX - moveX
             beforeY = lastY - moveY
-
+            
             if not is_inrange(beforeX, beforeY):
                 break
 
@@ -79,36 +78,82 @@ for t in range(1, m + 1):
                 pos[idx] = (lastX, lastY)
 
             lastX, lastY = beforeX, beforeY
-        
+
         points[closestIdx] += c
         pos[closestIdx] = (firstX, firstY)
         if is_inrange(firstX, firstY):
             board[firstX][firstY] = closestIdx
         else:
             is_live[closestIdx] = False
-    
+
     board[rudolf[0]][rudolf[1]] = -1
 
-    for i in range(1, p + 1):
+    for i in range(1, p+1):
         if not is_live[i] or stun[i] >= t:
             continue
-
-        minDist = (pos[i][0] - rudolf[0])** 2 + (pos[i][1] - rudolf[1]) ** 2
-        moveDir = -1
+        minDist = (pos[i][0] - rudolf[0])** 2 + ([pos[i][1]]- rudolf[1]) **2
+        movedir= -1
 
         for dir in range(4):
             nx = pos[i][0] + dx[dir]
-            ny = pos[i][0] + dy[dir]
-
-            if not is_inrange(nx, ny) or board[nx][ny] > 0:
+            ny = pos[i][1] + dy[dir]
+            
+            if not is_inrange(nx, ny) or board[nx][ny] >0:
                 continue
 
-            dist = ( nx - rudolf[0])** 2 + (ny - rudolf[1])**2
+            dist = (nx - rudolf[0]) **2 + (nx - rudolf[1]) **2
             if dist < minDist:
                 minDist = dist
-                movedir = dir
-        if moveDir != -1:
+                moveDir = dir
+        if movedir != -1:
+            nx = pos[i][0] + dx[moveDir]
+            ny = pos[i][1] + dy[moveDir]
 
+            if nx == rudolf[0] and ny == rudolf[1]:
+                stun[i] = t + 1
 
+                moveX = nx + moveX * d
+                moveY = ny + moveY * d
+                lastX, lastY = firstX, firstY
 
+                if d ==1:
+                    points[i] += d
+                else:
+                    while is_inrange(lastX, lastY) and board[lastX][lastY] > 0:
+                        lastX += moveX
+                        lastY += moveY
+                    
+                    while lastX != firstX or lastY != firstY:
+                        beforeX = lastX - moveX
+                        beforeY = lastY - moveY
+
+                        if not is_inrange(beforeX, beforeY):
+                            break
+
+                        idx = board[beforeX][beforeY]
+
+                        if not is_inrange(lastX, lastY):
+                            is_live[idx] = False
+                        else:
+                            board[lastX][lastY] = board[beforeX][beforeY]
+                            pos[idx] = (lastX, lastY)
+                        lastX, lastY = beforeX, beforeY
+
+                    points[i] += d
+                    board[pos[i][0]][pos[i][1]] = 0
+                    pos[i] = (firstX, firstY)
+                    if is_inrange(firstX, firstY):
+                        board[firstX][firstY] = i
+                    else:
+                        is_live[i] = False
+            else:
+                board[pos[i][0]][pos[i][0]] = 0
+                pos[i] = (nx,ny)
+                board[nx][ny] = i
+    for i  in range(1, p+1):
+        if is_live[i]:
+            points[i] += 1
+
+for i in range(1, p + 1):
+    print(points[i], end = " ")
 
