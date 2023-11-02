cd8400935be072161b841fb052bbd4e3c9125b4d
JunseokLEE <leeis327@naver.com>
2023-11-02 22:28:21 +0900

    u

---
+++ coding test/3회독/연습문제/8-3.py
@@ -0,0 +1,10 @@
+def pibo(x):
+    print('f(' + str(x) + ')', end = ' ')
+    if x == 1 or x ==2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = fibo(x - 1) + fibo(x - 2)
+    return d[x]
+
+
